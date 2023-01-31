d6940b69243c39916c67d0077fa2da7d021d2c0b
Egor <alliscakess@gmail.com>
2023-02-01 00:10:20 +0300

    set webpack with react and ts

---
+++ sdkjs-plugins/content/ready-docs/plugin/webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: '[name].[contenthash].js',
+    asyncChunks: true,
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx'],
+        },
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
---
+++ sdkjs-plugins/content/ready-docs/plugin/webpack.dev.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+const port = process.env.PORT || 3000
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    host: 'localhost',
+    static: './dist',
+    port: port,
+    historyApiFallback: true,
+    open: true,
+  },
+})
